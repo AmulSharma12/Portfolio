11e3518ea207077296fc871906e1cd403077e3c3
Amul Op <amulsharmarr67@gmail.com>
2025-01-14 00:48:37 +0530

    About page layout completed

---
+++ app/components/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { assets, infoList } from "@/assets/assets";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <>
+      <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+        <h1 className="font-Ovo text-center text-lg mb-2">Introduction</h1>
+        <h3 className="font-Ovo text-center text-5xl">About me</h3>
+
+        <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
+          <div className="w-64 sm:w-80  rounded-3xl max-w-none">
+            <Image
+              src={assets.user_image}
+              alt="user"
+              className="w-full rounded-3xl"
+            />
+          </div>
+
+          <div className="flex-1">
+            <p className="mb-10 max-w-2xl font-Ovo">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+              accusamus fugit laudantium nulla id ab commodi velit itaque
+              temporibus accusantium fugiat earum sequi doloremque explicabo,
+              aliquid totam. Cupiditate, ex consequatur.
+            </p>
+
+            <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+              {infoList.map(({ icon, iconDark, title, description }, index) => (
+                <li
+                  key={index}
+                  className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500"
+                >
+                  <Image src={icon} alt={title} className="w-7 mt-3 " />
+                  <h3 className="my-4 font-semibold text-gray-700 font-Ovo">
+                    {title}
+                  </h3>
+                  <span className="text-gray-600 font-Ovo text-sm">
+                    {description}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
